86c1204c68b8e987f29c16e829311d811e746bfd
Twinklehingu <twinkle.hingu29@gmail.com>
2024-11-04 13:38:27 -0800

    How to render a page

---
+++ src/Hello.js
@@ -0,0 +1,6 @@
+// function Hello(){
+//     return <h1>React Project!</h1>;
+// } 
+const Hello = () => <h1>React Project!</h1>;
+// export pi = 3.14;
+export default Hello;
---
+++ src/HelloClass.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+class HelloClass extends Comment{
+
+    render(){
+     
+        <h1>Hello Team, from class component </h1>
+    }
+}
+
+export default HelloClass; 
