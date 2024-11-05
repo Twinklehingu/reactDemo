eea3b391d9aa140c61d05234554fa02469f68e48
Twinklehingu <twinkle.hingu29@gmail.com>
2024-11-05 09:48:09 -0800

    updated code with state manage and how to bind data

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import './App.css';
 import Hello from './components/Hello';
 import HelloClass from './components/HelloClass';
 import Welcome from './components/Welcome';
+import Counter from './components/Counter';
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Welcome name="Super Man" price="123"></Welcome>
+        {/* <Welcome name="Super Man" price="123"></Welcome>
         <Welcome name="Spider Man" price="453"></Welcome>
         <Welcome name="Iron Man" price="523"></Welcome>
+        
+        */}
+
+        <Counter></Counter>
      
       </header>
     </div>
